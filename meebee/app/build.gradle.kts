2ce93cf4bb1cff6cdfafa6dc64829ab5055f7a53
chanho <uni070@naver.com>
2024-05-04 12:49:28 +0900

    [Feature] Project Gradle Setting

--- meebee/app/build.gradle.kts
+++ meebee/app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id ("kotlin-parcelize")
+    id ("kotlin-kapt")
 }
 
 android {
@@ -44,4 +46,15 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation ("com.squareup.retrofit2:converter-moshi:2.11.0")
+
+    implementation ("com.squareup.moshi:moshi:1.15.1")
+    kapt ("com.squareup.moshi:moshi-kotlin-codegen:1.15.1")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.10.0")
+
+
+    implementation ("androidx.fragment:fragment-ktx:1.7.0")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
 }
